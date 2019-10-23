d05bb562dff147c483606868fa9b361571706a1c
BG XPS <techydevil@gmail.com>
2019-10-23 20:26:39 +0530

    Added Bottom Navigation

---
+++ app/src/main/java/com/androar/kutnow/MainActivity.java
@@ -0,0 +1,51 @@
+package com.androar.kutnow;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle("Order");
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.nav_order:
+                    toolbar.setTitle("Order");
+                    return true;
+                case R.id.nav_trending:
+                    toolbar.setTitle("Trending");
+                    return true;
+                case R.id.nav_contact:
+                    toolbar.setTitle("Contact");
+                    return true;
+            }
+            return false;
+        }
+    };
+}
